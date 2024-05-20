b6e9e11b2201f283c863da50caf522ba5ffea39d
wldnn021200 <welldonestoree@gmail.com>
2024-05-20 19:24:36 +0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function tambah() {
-    var bilangan1 = parseInt(document.getElementById("bilangan1").value);
-    var bilangan2 = parseInt(document.getElementById("bilangan2").value);
-    
-    var hasil = bilangan1 + bilangan2;
-    
-    document.getElementById("hasil").innerHTML = "Hasil penjumlahan: " + hasil;
-}
